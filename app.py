13b35e883fd3ce7383120ce1d89d201213f9cdd8
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-01-04 19:27:54 +0000

    Connected loginpage to mongodb.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, render_template, redirect, 
+    Flask, render_template, redirect,
     request, session, url_for, flash)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -29,9 +29,27 @@ def base():
     return render_template("base.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}!".format(request.form.get("username")))
+            else:
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
+
 
 
 @app.route("/register", methods=["GET", "POST"])
