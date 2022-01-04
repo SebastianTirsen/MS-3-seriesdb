2ddfd2fba069a5fd338f86f6d3cd64f5e5fe8f78
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-01-04 12:29:38 +0000

    Connected registerpage to user db.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     request, session, url_for, flash)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -33,8 +34,26 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "name": request.form.get("name").lower(),
+            "email": request.form.get("email").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] =request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 
