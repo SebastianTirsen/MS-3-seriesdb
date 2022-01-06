55b0821fabb46e687984820eb0dc84f6fed1fa14
Sebastian Tirsén <sebastian.tirsen@gmail.com>
2022-01-06 13:24:37 +0000

    Added function logout, stars to the reviews, and headings.

--- app.py
+++ app.py
@@ -99,7 +99,19 @@ def contact():
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 
 @app.route("/show")
